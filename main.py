da4707a3ae8e4a310022eeef026b2590be5ff484
Dávid Gábor Bodor <david.gabor.bodor@gmail.com>
2012-12-05 20:59:20 +0100

    Expand to snowflakes demo.
    Snowflakes kind of look like jellyfish right now.
    Some variation in movement and sprites would also be nice.

--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 
 import pyglet
 
+from random import random, randint
+
 window = pyglet.window.Window()
 
 pyglet.resource.path = ['data']
 pyglet.resource.reindex()
 
 arrow = pyglet.resource.image('white-arrow.png')
+snowflake = pyglet.resource.image('white-arrow.png')
+
 bullet = pyglet.sprite.Sprite(arrow)
 
+max_snowflakes = 1000
+snowflakes = []
 
 @window.event
 def on_resize(w, h):
@@ -19,8 +25,23 @@ def on_resize(w, h):
 def on_draw():
     window.clear()
     bullet.draw()
+    for s in snowflakes:
+        s.draw()
 
 def update(dt):
+    if len(snowflakes) < max_snowflakes:
+        snowflakes.append(pyglet.sprite.Sprite(
+            snowflake,
+            x=randint(0, window.width),
+            y=randint(0, window.height)))
+
+    for s in snowflakes:
+        s.x += random() - 0.5
+        s.y -= random()
+        s.x %= window.width
+        s.y %= window.height
+        s.rotation += random() - 0.5
+
     bullet.x += 3
     bullet.y += 2
     bullet.x %= window.width
